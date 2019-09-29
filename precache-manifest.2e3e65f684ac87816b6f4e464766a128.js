e6a9d241ccbacc595e140ef67ff829e875c9abe0
nurA <arun@honeybadgerlabs.in>
2019-09-29 17:31:22 +0530

    Updates

---
+++ precache-manifest.2e3e65f684ac87816b6f4e464766a128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8094d2a37017d8d6d2c9493a7c02ad22",
+    "url": "/job-track/index.html"
+  },
+  {
+    "revision": "67c7def0c07d6e611ea6",
+    "url": "/job-track/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d70cf004628967560ea2",
+    "url": "/job-track/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "67c7def0c07d6e611ea6",
+    "url": "/job-track/static/js/2.e4cf1b63.chunk.js"
+  },
+  {
+    "revision": "d70cf004628967560ea2",
+    "url": "/job-track/static/js/main.b30fd2f1.chunk.js"
+  },
+  {
+    "revision": "9ecdea69ffe9dae8dc2c",
+    "url": "/job-track/static/js/runtime~main.13caacdd.js"
+  }
+]);
